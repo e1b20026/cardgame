9bd82228a74bb46d78d403ffda75920b2130e172
e1b20026 <e1b20026@oit.ac.jp>
2022-11-08 16:04:31 +0900

    リポジトリの初期化

---
+++ src/main/java/playingcards/cardgame/CardgameApplication.java
@@ -0,0 +1,13 @@
+package playingcards.cardgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/playingcards/cardgame/CardgameApplicationTests.java
@@ -0,0 +1,13 @@
+package playingcards.cardgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
