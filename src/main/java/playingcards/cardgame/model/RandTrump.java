073f9580636e34edda4ca596505dfb063f29e023
e1b20026 <e1b20026@oit.ac.jp>
2022-11-29 16:01:55 +0900

    3枚のランダムカードのセッティング

---
+++ src/main/java/playingcards/cardgame/model/RandTrump.java
@@ -0,0 +1,35 @@
+package playingcards.cardgame.model;
+
+public class RandTrump {
+  int id;
+  String suit;
+  int number;
+
+  public RandTrump() {
+
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getSuit() {
+    return suit;
+  }
+
+  public void setSuit(String suit) {
+    this.suit = suit;
+  }
+
+  public int getNumber() {
+    return number;
+  }
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+}
