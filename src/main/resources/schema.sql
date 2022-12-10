2b1fc280fe3d4db55178a8e5d8868a006dfea8f1
e1b20015 <e1b20015@oit.ac.jp>
2022-12-10 16:25:14 +0900

    結果を格納するためのデータベースを作成

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -17,3 +17,19 @@ CREATE TABLE randtrump (
   number VARCHAR,
   primary key(id)
 );
+
+CREATE TABLE userresult (
+  id INT,
+  username VARCHAR UNIQUE,
+  tn1 VARCHAR NOT NULL,
+  ts1 VARCHAR NOT NULL,
+  tn2 VARCHAR NOT NULL,
+  ts2 VARCHAR NOT NULL,
+  tn3 VARCHAR NOT NULL,
+  ts3 VARCHAR NOT NULL,
+  tn4 VARCHAR NOT NULL,
+  ts4 VARCHAR NOT NULL,
+  tn5 VARCHAR NOT NULL,
+  ts5 VARCHAR NOT NULL,
+  primary key(id)
+);
