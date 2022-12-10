6d03d19e908a5feac0c0d39bcb3637356ebf0d93
e1b20026 <e1b20026@oit.ac.jp>
2022-12-10 15:38:56 +0900

    AllTrumpClass内のRoyalStraghtメソッドを実装

--- src/main/java/playingcards/cardgame/model/AllTrump.java
+++ src/main/java/playingcards/cardgame/model/AllTrump.java
@@ -22,4 +22,17 @@ public class AllTrump {
     this.suit = suit;
     this.evaluationNumber = evaluationNumber;
   }
+
+  public static ArrayList<Trump1> RoyalStraight(ArrayList<Trump1> trump) {
+    ArrayList<Trump1> royalStraight = new ArrayList<>();
+    ArrayList<Trump1> Straight = Straight(trump);
+    ArrayList<Trump1> Frash = Frash(Straight);
+
+    if (Straight.size() != 0) {// ストレートの時
+      if (Straight.get(Straight.size() - 1).evaluationNumber == 10 && Frash.size() != 0) {
+        royalStraight = Frash;
+      }
+    }
+    return royalStraight;
+  }
 }
