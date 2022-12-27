02f69837b0adcab9a42dda1c27c982624006cb36
e1b20015 <e1b20015@oit.ac.jp>
2022-12-27 14:49:28 +0900

    RandTrumpMapperのselect文の追加

--- src/main/java/playingcards/cardgame/model/RandTrumpMapper.java
+++ src/main/java/playingcards/cardgame/model/RandTrumpMapper.java
@@ -17,6 +17,9 @@ public interface RandTrumpMapper {
   @Select("SELECT * from randtrump where id = #{id}")
   RandTrump selectIdRandTrump(int id);
 
+  @Select("SELECT * from randtrump where suit is not null")
+  RandTrump selectIdRandTrumpNull();
+
   @Update("UPDATE randtrump SET suit = #{trump.suit},  number = #{trump.number} where id = #{id}")
   boolean updateIdRandTrump(Trump trump, int id);
 }
