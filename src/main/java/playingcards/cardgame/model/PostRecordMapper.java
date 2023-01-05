877628205c81f0c6d06935c83ed3afe211f555e1
e1b20032 <e1b20032@oit.ac.jp>
2023-01-05 15:34:10 +0900

    Controller.javaのround4の変更(途中)

--- src/main/java/playingcards/cardgame/model/PostRecordMapper.java
+++ src/main/java/playingcards/cardgame/model/PostRecordMapper.java
@@ -13,4 +13,7 @@ public interface PostRecordMapper {
 
   @Select("select * from PostRecord")
   ArrayList<PostRecord> selectAllPostRecord();
+
+  @Select("select * from PostRecord where id = #{id}")
+  ArrayList<PostRecord> selectIdPostRecord(int id);
 }
